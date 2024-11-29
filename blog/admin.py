2bba038b1567a61e1b111e28af590906436dace6
John Gardner <john.gardner@msn.com>
2024-11-29 17:13:44 +0000

    config django-summernote

--- blog/admin.py
+++ blog/admin.py
@@ -2,15 +2,16 @@ from django.contrib import admin
 from .models import BlogPost, BlogPostComments
 from django_summernote.admin import SummernoteModelAdmin
 
-class BlogPostAdmin(SummernoteModelAdmin):
+# @admin.register(BlogPost) # This decorator registers the custom admin class for BlogPost, compared to the traditional approach of registering a model (as below)
+# class BlogPostAdmin(SummernoteModelAdmin): # ... admin configuration for BlogPost ...
 
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
-    prepopulated_fields = {'slug': ('title',)}
-    summernote_fields = ('content',)
+#     list_display = ('title', 'slug', 'status')
+#     search_fields = ['title']
+#     list_filter = ('status',)
+#     prepopulated_fields = {'slug': ('title',)}
+#     summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(BlogPost) #This registion allows me to create, update and delete blog posts from the admin panel.
+admin.site.register(BlogPost) # no longer needed as decorator does this above
 admin.site.register(BlogPostComments) #This registion allows users to create, comments on my blog posts.
 
