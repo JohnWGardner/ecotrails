fd0be317794dbe582fa0c56dfa395dd28e9e96ac
John Gardner <john.gardner@msn.com>
2024-11-29 15:40:53 +0000

    Building the homepage

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     #path('', views.HomePage.as_view(), name='home'),
-    path('', views.BlogPostList.as_view(), name='home'),
+    path('blog/', views.BlogPostList.as_view(), name='home'),
 ]
 
