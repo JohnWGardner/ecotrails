e3f45b94442e33106e0c051ce4746cf8753daa23
John Gardner <john.gardner@msn.com>
2024-12-03 13:55:03 +0000

    added post_detail view

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     #path('', views.HomePage.as_view(), name='home'),
     path('blog/', views.BlogPostList.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
 from django.views import generic
 from .models import BlogPost
@@ -7,5 +7,28 @@ from .models import BlogPost
 class BlogPostList(generic.ListView): # Defines a class-based view for listing each blog post
     # model = BlogPost # This is made redundant by the queryset explicitly stating all published (1) posts are displayed.
     queryset = BlogPost.objects.filter(status=1) # Fetches all BlogPost objects from the database
-    template_name = "blog/index.html"
-    paginate_by = 9
+    template_name = "blog/index.html" #explicit naming of template name
+    paginate_by = 6 # how many blogs are shown per page 
+
+def post_detail(request, slug): #Display an individual :model:`blog.Post`.
+    """
+    
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.BlogPost`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = BlogPost.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
