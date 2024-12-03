1ece4e7a29d75207fe98ef3c4e5b1920c4ef45ff
John Gardner <john.gardner@msn.com>
2024-12-03 13:32:23 +0000

    enable serving of static files

--- ecotrails/settings.py
+++ ecotrails/settings.py
@@ -32,7 +32,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-johnwgardner-ecotrails-19v3sb3fnq6.ws.codeinstitute-ide.net',  # Match localhost (for development)
