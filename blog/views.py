18c93a097e5b54a5d51c8f1a3762fdd63aeaae5b
John Gardner <john.gardner@msn.com>
2024-12-03 11:57:37 +0000

    add blog_index&base

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import BlogPost
 # Create your views here.
 
 class BlogPostList(generic.ListView): # Defines a class-based view for listing each blog post
-    # model = BlogPost # This is made redundant by the queryset explicitly stating all posts are displayed.
+    # model = BlogPost # This is made redundant by the queryset explicitly stating all published (1) posts are displayed.
     queryset = BlogPost.objects.filter(status=1) # Fetches all BlogPost objects from the database
-    template_name = 'blog/blog_post_list.html' # optional addition for explicit naming, as i haven't followed blogpost_list default naming pattern
-
+    template_name = "blog/index.html"
+    paginate_by = 6
