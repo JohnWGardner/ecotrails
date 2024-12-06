9e7d85c6bf07808f55e700b693daea4f53e783f3
John Gardner <john.gardner@msn.com>
2024-12-06 12:45:45 +0000

    added contact page

--- contact/views.py
+++ contact/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
 from .models import Contact
+from .forms import CollaborateForm
 
 # Create your views here.
 
 def contact_me(request):
     """
-    Renders the About page
+    Renders the Contact page
     """
     contact = Contact.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "contact/contact.html",
-        {"contact": contact},
+        {"contact": contact, "collaborate_form": collaborate_form},
+        
     )
