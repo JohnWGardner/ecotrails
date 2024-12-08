5d9b8428695d46c3fd0630ae650542c1dbaa6fe7
John Gardner <john.gardner@msn.com>
2024-12-08 16:34:09 +0000

    updating destinations model/view

--- destinations/views.py
+++ destinations/views.py
@@ -6,7 +6,7 @@ from .models import Destination
 
 class DestinationList(generic.ListView):
     queryset = Destination.objects.filter(status=1).order_by('-created_on')
-    template_name = "destinations/index.html"
+    template_name = "destinations/destination_list.html"
     paginate_by = 6
 
 def destination_detail(request, slug):
