20223b6ea87f094db1f5576b9820a691773d2e0b
John Gardner <john.gardner@msn.com>
2024-12-09 09:18:07 +0000

    correcting destinations issues

--- destinations/views.py
+++ destinations/views.py
@@ -7,6 +7,17 @@ class DestinationList(generic.ListView):
     template_name = "destinations/destination_list.html"
     paginate_by = 6
 
+    def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            destinations_by_continent = {}
+            for destination in self.get_queryset():
+                continent = destination.get_continent_display()
+                if continent not in destinations_by_continent:
+                    destinations_by_continent[continent] = []
+                destinations_by_continent[continent].append(destination)
+            context['destinations_by_continent'] = destinations_by_continent
+            return context
+
 def destination_detail(request, slug):
     queryset = Destination.objects.filter(status=1)
     destination = get_object_or_404(queryset, slug=slug)
